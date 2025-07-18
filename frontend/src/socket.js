04e290e05cc561adc200c01ddb878d94f1f4f45e
Kapil Dagar <kapildagar600@gmail.com>
2025-07-19 00:22:14 +0530

    Update frontend to use deployed backend

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,2 +1,3 @@
 import { io } from 'socket.io-client';
-export const socket = io('http://localhost:5000'); // Change for production
+// export const socket = io('http://localhost:5000'); // Change for production
+export const socket = io(import.meta.env.VITE_API_BASE_URL); // Change for production
